bcd377d39bca9f23fc2f218ec203f2ff102bcfda
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-20 12:20:11 +0200

    Prepare server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+app.get('/contact', (req, res) => {
+  res.show('contact.html');
+});
+app.get('/info', (req, res) => {
+  res.show('info.html');
+});
+app.get('/history', (req, res) => {
+  res.show('history.html');
+});
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+});
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
