26becf916bd4f478ecd054d9cc55b5a2c9003242
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-20 14:22:43 +0200

    Implement handlebars

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+const hbs = require('express-handlebars');
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false });
 });
+
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
 });
+
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: false });
 });
+
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: false });
 });
+
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', { layout: false });
 });
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
